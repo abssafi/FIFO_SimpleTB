ee87f3797d8c7dc8fbc1167ed91d534fcd25b244
abssafi <129922695+abssafi@users.noreply.github.com>
2024-09-28 19:23:23 +0500

    Update top.sv

--- sv/top.sv
+++ sv/top.sv
@@ -1,62 +1,24 @@
-#include "ifa"
-module fifo (ifa.fifoport mbus);
+module top;
+// SYSTEMVERILOG: timeunit and timeprecision specification
+timeunit 1ns;
+timeprecision 1ns;
 
-// Internal variables
-  reg [mbus.DATA_WIDTH-1:0] buffer[0:mbus.DEPTH-1]; // FIFO buffer
-  reg [$clog2(mbus.DEPTH):0] count = 0; // Number of items in FIFO, extra bit for full detection
-  reg [$clog2(mbus.DEPTH)-1:0] wr_ptr = 0; // Write pointer
-  reg [$clog2(mbus.DEPTH)-1:0] rd_ptr = 0; // Read pointer
+// SYSTEMVERILOG: logic and bit data types
+bit         clk;
+  bit         rst_n;
+  ifa memory_bus(clk, rst_n);
+  
+assign rst_n = 1;
+// SYSTEMVERILOG:: implicit .* port connections.
+  test_fifo test (.tbus(memory_bus));
 
-// Full and empty logic
-  assign mbus.full = (count == mbus.DEPTH);
-assign mbus.empty = (count == 0);
+// SYSTEMVERILOG:: implicit .name port connections
+  fifo block ( .mbus(memory_bus));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+  end
 
-
-// Simultaneous read and write handling
-  always @(posedge mbus.clk or negedge mbus.rst_n) begin
-    if (!mbus.rst_n) begin
-        // Asynchronous reset
-        wr_ptr <= 0;
-        rd_ptr <= 0;
-        count <= 0;
-        mbus.overflow <= 0;
-        mbus.underflow <= 0;
-    end else begin
-        // Default the flags to 0
-        mbus.overflow <= 0;
-        mbus.underflow <= 0;
-        
-        // Check for write attempt when FIFO is full
-      if (mbus.wr_en && mbus.full) begin
-            mbus.overflow <= 1;
-        end
-
-        // Check for read attempt when FIFO is empty
-      if (mbus.rd_en && mbus.empty) begin
-            mbus.underflow <= 1;
-        end
-
-        // Perform write operation if enabled and not full
-      if (mbus.wr_en && !mbus.full) begin
-        buffer[wr_ptr] <= mbus.data_in;
-        wr_ptr <= (wr_ptr + 1) % mbus.DEPTH;
-            // Only increment count if a read is not also happening
-        if (!mbus.rd_en || mbus.empty) begin
-                count <= count + 1;
-            end
-        end
-
-        // Perform read operation if enabled and not empty
-      if (mbus.rd_en && !mbus.empty) begin
-            mbus.data_out <= buffer[rd_ptr];
-        rd_ptr <= (rd_ptr + 1) % mbus.DEPTH;
-            // Only decrement count if a write is not also happening
-        if (!mbus.wr_en || mbus.full) begin
-                count <= count - 1;
-            end
-        end
-    end
-end
-
-
+always #5 clk = ~clk;
 endmodule
